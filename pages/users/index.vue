9c287ad700da6463502a4420ffed41c3977fe999
take8 <37664118+take8@users.noreply.github.com>
2020-01-30 04:05:15 +0900

    Add user list

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="container">
     <h1>/users/index</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.id }}, {{ user.name }}, {{ user.company.name }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+const axios = require('axios')
+const url = 'https://jsonplaceholder.typicode.com/users'
+
+export default {
+  asyncData({ params }) {
+    return axios.get(url).then((res) => {
+      return { users: res.data }
+    })
+  }
+}
+</script>
