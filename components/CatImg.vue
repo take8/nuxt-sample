a4a82d5d8cef2543c93b59b4f9994e343c8b2880
take8 <37664118+take8@users.noreply.github.com>
2020-02-02 23:26:46 +0900

    Add assets file (image)

---
+++ components/CatImg.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <img src="~/assets/cat.jpg" alt="猫" />
+  </div>
+</template>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -6,14 +6,20 @@
         {{ user.id }}, {{ user.name }}, {{ user.company.name }}
       </li>
     </ul>
+    <cat-img />
   </div>
 </template>
 
 <script>
+import CatImg from '~/components/CatImg.vue'
+
 const axios = require('axios')
 const url = 'https://jsonplaceholder.typicode.com/users'
 
 export default {
+  components: {
+    CatImg
+  },
   asyncData({ params, error }) {
     return axios
       .get(url)
