5a9aced4367597985233fb5fb3e7eedec0c0192b
take8 <37664118+take8@users.noreply.github.com>
2020-02-03 01:10:21 +0900

    Convert to Module mode

--- pages/store.vue
+++ pages/store.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="container">
-    <p>{{ $store.state.message }}</p>
+    <!-- <p>{{ $store.state.message }}</p> -->
+    <p>{{ $store.state.hello.message }}</p>
     <!-- <button v-on:click="$store.commit('updateMessage')">Update</button> -->
     <!-- <button v-on:click="$store.commit('updateMessage', 'Commit with payload')">
       Update
     </button> -->
-    <button
+    <!-- <button
       v-on:click="
         $store.dispatch('updateMessageAction', 'Dispatch with payload')
+      "
+    >
+      Update
+    </button> -->
+    <button
+      v-on:click="
+        $store.dispatch('hello/updateMessageAction', 'Dispatch with payload')
       "
     >
       Update
