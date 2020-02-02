801c6a7d48e31f8fb813949e39aa08455af19dfb
take8 <37664118+take8@users.noreply.github.com>
2020-02-03 02:21:16 +0900

    Add Couner app

---
+++ components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="counter">
+    <h2>{{ title }}</h2>
+    <div>
+      <span
+        >Counter: <span>{{ $store.state.counter.count }}</span>
+      </span>
+      <button v-on:click="$store.commit('counter/increment')">+1</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      title: 'Counter'
+    }
+  }
+}
+</script>
---
+++ pages/counter/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <ul>
+      <li><counter /></li>
+      <li><counter /></li>
+      <li><counter /></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Counter from '~/components/Counter'
+
+export default {
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+ul {
+  padding: 0;
+}
+li {
+  list-style: none;
+}
+
+li {
+  border-bottom: 2px solid #999;
+}
+li:last-of-type {
+  border-bottom: none;
+}
+</style>
